f1c7ddfbcf2c2f1cfe6e6a9d15ee7a2f14d5576f
Tom Dra <tomekdrabek@interia.pl>
2020-05-12 22:30:12 +0200

    Run build v2

---
+++ build/precache-manifest.6130dbcfa33a61fda338f60a39eae9df.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff70266452cc9dfb1cbc692b6f2ce31f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7e347f262b8b6224cf8",
+    "url": "./static/css/2.11833122.chunk.css"
+  },
+  {
+    "revision": "faf6a1bbbcee33a45643",
+    "url": "./static/css/main.f897c7ec.chunk.css"
+  },
+  {
+    "revision": "a7e347f262b8b6224cf8",
+    "url": "./static/js/2.a69ae3fe.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "./static/js/2.a69ae3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf6a1bbbcee33a45643",
+    "url": "./static/js/main.39fbb476.chunk.js"
+  },
+  {
+    "revision": "8114a27521c2212da4d2",
+    "url": "./static/js/runtime-main.494bfbf7.js"
+  },
+  {
+    "revision": "e358556372a01e2e52f19bf7e71ebe4e",
+    "url": "./static/media/big-logo.e3585563.jpg"
+  },
+  {
+    "revision": "34b3eff48da92910e93ea20b0a2d97ac",
+    "url": "./static/media/depresja-dzieci-712x515.34b3eff4.jpg"
+  },
+  {
+    "revision": "ebef9577a92b448bb11fce1d40f0133f",
+    "url": "./static/media/fejk.ebef9577.png"
+  },
+  {
+    "revision": "ea7dcf0ece810bee07f3f8bd7b4bb684",
+    "url": "./static/media/girl-447701.ea7dcf0e.jpg"
+  },
+  {
+    "revision": "6eda72ff2ce5b3e96d8222a572003ff4",
+    "url": "./static/media/gravel.6eda72ff.png"
+  },
+  {
+    "revision": "6cae162d7fcc87142aa278e0ea59cf07",
+    "url": "./static/media/statystyki.6cae162d.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.737b9938d65f31deb7a4fedfe2a955a4.js"
+  "./precache-manifest.6130dbcfa33a61fda338f60a39eae9df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
